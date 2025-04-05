2f564e076b5f05c4b02900df896634fe2b1cf4da
Fadoon T <fadoon.tar@gmail.com>
2025-04-05 22:45:59 +0100

    Added global error handler

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from 'express';
+import cookieParser from 'cookie-parser'
 import { PORT } from './config/env.js';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDB from './database/db.js';
+import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send('<h1>Welcome to the subscription API<h1/>');

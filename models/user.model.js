f901a993b494041f27e5f228cacd04b5fbde51f3
Fadoon T <fadoon.tar@gmail.com>
2025-04-05 23:53:34 +0100

    Auth setup

--- models/user.model.js
+++ models/user.model.js
@@ -1,28 +1,27 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Username is required'],
-        trim: true,
-        minLength: 2,
-        maxLength: 50
-    },
-    email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        trim: true,
-        match: [/\S+@\.\S+/, 'Please provide a valid email address'],     
-    },
-    password: {
-        type: String,
-        required: [true, 'User password is required'],
-        minLength: 6,
-    }
-
-}, {timestamps: true})
-
+  name: {
+    type: String,
+    required: [true, 'User Name is required'],
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, 'User Email is required'],
+    unique: true,
+    trim: true,
+    lowercase: true,
+    match: [/\S+@\S+\.\S+/, 'Please provide a valid email address'],
+  },
+  password: {
+    type: String,
+    required: [true, 'User Password is required'],
+    minLength: 6,
+  }
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
